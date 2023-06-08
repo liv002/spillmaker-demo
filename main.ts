08d43e005dc5e212f9fcbfb578ef6bc8077055da
liv002 <43001067+liv002@users.noreply.github.com>
2023-06-08 15:18:23 +0200

    Fixed minor sprite change when moving, and fixed bigboi hp

--- main.ts
+++ main.ts
@@ -7,11 +7,68 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.boss, function (sprite, otherSpr
     music.play(music.melodyPlayable(music.bigCrash), music.PlaybackMode.UntilDone)
     info.changeLifeBy(-2)
 })
+function createBigboi () {
+    bigboi = sprites.create(img`
+        .................111111111111111.................
+        ...............11ccccccccccccccc11...............
+        .............11ccccccccccccccccccc11.............
+        ...........11ccccccccccccccccccccccc11...........
+        .........11ccccccccccccccccccccccccccc11.........
+        ........1ccccccccccccccccccccccccccccccc1........
+        .......1ccccccccccccccccccccccccccccccccc1.......
+        ......1ccccccccccccccccccccccccccccccccccc1......
+        .....1ccccccccccccccccccccccccccccccccccccc1.....
+        ....1cccccccccccccccaaaaaccccccccccccccccccc1....
+        ....1ccccccccccccccacccccacccccccccccccccccc1....
+        ...1cccccccccccccccacccccaccccccccccccccccccc1...
+        ...1cccccccccccccccacccccaccccccccccccccccccc1...
+        ..1ccccccccccccccccacccccacccccccccccccccccccc1..
+        ..1ccccccccccccccccacccccacccccccccccccccccccc1..
+        .1ccccccccccccccccccaaaaacccccccccccccccccccccc1.
+        .1ccccccccccccccccccccccccccccccccccccccccccccc1.
+        1ccccccccccccccccccccccccccccccccccccccccccccccc1
+        1cccaaaccccccccccccccccccccccccccccccccccccccccc1
+        1cccacaccccccccccccccccccccccccccccccccccccccccc1
+        1cccaaaccccccccccccccccccccccccccccccccccccccccc1
+        1ccccccccccccccccccccccccccccccccccccccccccccccc1
+        1ccccccccccccccccccccccccccccccccccccccccccccccc1
+        1ccccccccccccccccccccccccccccccccccccccccccccccc1
+        1ccccccccccccccccccccccccccccccccccccccccccccccc1
+        1ccccccccccccccccccccccccccccccccccccccccccccccc1
+        1ccccccccccccccccccccccccccccccccccccccccccccccc1
+        1cccccccccccccaaaaaaaccccccccccccccccccccccccccc1
+        1ccccccccccccacccccccaccccccccccaaaaaccccccccccc1
+        1cccccccccccacccccccccaccccccccacccccacccccccccc1
+        1ccccccccccacccccccccccacccccccacccccacccccccccc1
+        1ccccccccccacccccccccccacccccccacccccacccccccccc1
+        .1cccccccccacccccccccccacccccccacccccaccccccccc1.
+        .1cccccccccacccccccccccacccccccacccccaccccccccc1.
+        ..1ccccccccacccccccccccaccccccccaaaaaccccccccc1..
+        ..1ccccccccacccccccccccacccccccccccccccccccccc1..
+        ...1cccccccacccccccccccaccccccccccccccccccccc1...
+        ...1ccccccccacccccccccacccccccccccccccccccccc1...
+        ....1ccccccccacccccccacccccccccccccccccccccc1....
+        ....1cccccccccaaaaaaaccccccccccccccccccccccc1....
+        .....1ccccccccccccccccccccccccccccccccccccc1.....
+        ......1ccccccccccccccccccccccccccccccccccc1......
+        .......1ccccccccccccccccccccccccccccccccc1.......
+        ........1ccccccccccccccccccccccccccccccc1........
+        .........11ccccccccccccccccccccccccccc11.........
+        ...........11ccccccccccccccccccccccc11...........
+        .............11ccccccccccccccccccc11.............
+        ...............11ccccccccccccccc11...............
+        .................111111111111111.................
+        `, SpriteKind.boss)
+    bigboi.setPosition(75, -20)
+    bigboi.setVelocity(0, 10)
+    bigboi.setFlag(SpriteFlag.AutoDestroy, true)
+    list.push(10)
+}
 controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (10 <= info.score()) {
+    if (5 <= info.score()) {
         mySprite.startEffect(effects.hearts, 2000)
         music.play(music.melodyPlayable(music.powerUp), music.PlaybackMode.UntilDone)
-        info.changeScoreBy(-10)
+        info.changeScoreBy(5)
         info.changeLifeBy(1)
     }
 })
@@ -36,26 +93,47 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
         `, mySprite, 0, -150)
     music.play(music.melodyPlayable(music.pewPew), music.PlaybackMode.UntilDone)
 })
-function asteroidLogic (astroid: Sprite) {
+function createAsteroid () {
+    asteroide = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . b b . . 1 1 1 1 1 . . . b . 
+        . . b b . 1 b b b b b 1 . . . . 
+        . . . . 1 b b b b b b b 1 . . . 
+        . . . 1 b b c b b b b b b 1 . . 
+        . . 1 b b b b b b c b b b b 1 . 
+        . . 1 b b b b b b b b b b b 1 . 
+        . . 1 b b c b b b b b b b b 1 . 
+        . . 1 b b c c b b b b b b b 1 . 
+        . . 1 b b b b b b b b c b b 1 . 
+        . . . 1 b b b b b b c c b 1 . . 
+        . . . . 1 b b b b b b b 1 . . . 
+        . . b b . 1 b b b b b 1 . . b . 
+        . . . b . . 1 1 1 1 1 . b b b . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
     speed = randint(50, 100) + game.runtime() / 1000
-    astroid.setVelocity(0, speed)
-    astroid.setPosition(randint(3, 150), 0)
+    asteroide.setVelocity(0, speed)
+    asteroide.setPosition(randint(3, 150), 0)
+    asteroide.setFlag(SpriteFlag.AutoDestroy, true)
 }
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.boss, function (sprite, otherSprite) {
+    bigboiHP = list[0]
     if (bigboiHP == 1) {
+        list.shift()
         sprites.destroy(otherSprite, effects.disintegrate, 2000)
         sprites.destroy(sprite)
         music.play(music.melodyPlayable(music.magicWand), music.PlaybackMode.UntilDone)
         info.changeScoreBy(5)
         info.changeLifeBy(1)
     } else {
+        list[0] = bigboiHP - 1
         animation.runMovementAnimation(
         otherSprite,
         animation.animationPresets(animation.shake),
         200,
         false
         )
-        bigboiHP += -1
         music.play(music.melodyPlayable(music.thump), music.PlaybackMode.UntilDone)
         sprites.destroy(sprite)
     }
@@ -72,11 +150,12 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     music.play(music.melodyPlayable(music.smallCrash), music.PlaybackMode.UntilDone)
     info.changeLifeBy(-1)
 })
-let asteroide: Sprite = null
-let bigboi: Sprite = null
 let bigboiHP = 0
 let speed = 0
+let asteroide: Sprite = null
 let projectile: Sprite = null
+let bigboi: Sprite = null
+let list: number[] = []
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
     ........b........
@@ -105,83 +184,88 @@ controller.moveSprite(mySprite)
 mySprite.setStayInScreen(true)
 mySprite.bottom = 120
 info.setLife(3)
+list = []
+game.onUpdateInterval(100, function () {
+    if (controller.A.isPressed() || controller.B.isPressed()) {
+        mySprite.setImage(img`
+            ........b........
+            .......bbb.......
+            .......b2b.......
+            .......322.......
+            ......32222......
+            ......32222......
+            .....3211922.....
+            .....3119992.....
+            .b...3299922...b.
+            .b...3222222...b.
+            .c..322199222..c.
+            .c.32211996222.c.
+            33322119999622222
+            .222222222222222.
+            .222222222222222.
+            .e2e2e2e2e2e2e2e.
+            ..e2e2e2e2e2e2e..
+            ...ccc.....ccc...
+            ..bbbbb...bbbbb..
+            .................
+            `)
+    } else if (controller.anyButton.isPressed()) {
+        mySprite.setImage(img`
+            ........b........
+            .......bbb.......
+            .......b2b.......
+            .......322.......
+            ......32222......
+            ......32222......
+            .....3211922.....
+            .....3119992.....
+            .b...3299922...b.
+            .b...3222222...b.
+            .c..322199222..c.
+            .c.32211996222.c.
+            33322119999622222
+            .222222222222222.
+            .222222222222222.
+            .e2e2e2e2e2e2e2e.
+            ..e2e2e2e2e2e2e..
+            ...ccc.....ccc...
+            ..bbbbb...bbbbb..
+            ...414.....414...
+            ..45114...45114..
+            ..45114...45114..
+            ..45514...45544..
+            ..4554.....454...
+            ...44......44....
+            `)
+    } else {
+        mySprite.setImage(img`
+            ........b........
+            .......bbb.......
+            .......b2b.......
+            .......322.......
+            ......32222......
+            ......32222......
+            .....3211922.....
+            .....3119992.....
+            .b...3299922...b.
+            .b...3222222...b.
+            .c..322199222..c.
+            .c.32211996222.c.
+            33322119999622222
+            .222222222222222.
+            .222222222222222.
+            .e2e2e2e2e2e2e2e.
+            ..e2e2e2e2e2e2e..
+            ...ccc.....ccc...
+            ..bbbbb...bbbbb..
+            .................
+            `)
+    }
+})
 game.onUpdateInterval(500 - game.runtime() / 1000, function () {
     if (Math.percentChance(3)) {
-        bigboi = sprites.create(img`
-            .................111111111111111.................
-            ...............11ccccccccccccccc11...............
-            .............11ccccccccccccccccccc11.............
-            ...........11ccccccccccccccccccccccc11...........
-            .........11ccccccccccccccccccccccccccc11.........
-            ........1ccccccccccccccccccccccccccccccc1........
-            .......1ccccccccccccccccccccccccccccccccc1.......
-            ......1ccccccccccccccccccccccccccccccccccc1......
-            .....1ccccccccccccccccccccccccccccccccccccc1.....
-            ....1cccccccccccccccaaaaaccccccccccccccccccc1....
-            ....1ccccccccccccccacccccacccccccccccccccccc1....
-            ...1cccccccccccccccacccccaccccccccccccccccccc1...
-            ...1cccccccccccccccacccccaccccccccccccccccccc1...
-            ..1ccccccccccccccccacccccacccccccccccccccccccc1..
-            ..1ccccccccccccccccacccccacccccccccccccccccccc1..
-            .1ccccccccccccccccccaaaaacccccccccccccccccccccc1.
-            .1ccccccccccccccccccccccccccccccccccccccccccccc1.
-            1ccccccccccccccccccccccccccccccccccccccccccccccc1
-            1cccaaaccccccccccccccccccccccccccccccccccccccccc1
-            1cccacaccccccccccccccccccccccccccccccccccccccccc1
-            1cccaaaccccccccccccccccccccccccccccccccccccccccc1
-            1ccccccccccccccccccccccccccccccccccccccccccccccc1
-            1ccccccccccccccccccccccccccccccccccccccccccccccc1
-            1ccccccccccccccccccccccccccccccccccccccccccccccc1
-            1ccccccccccccccccccccccccccccccccccccccccccccccc1
-            1ccccccccccccccccccccccccccccccccccccccccccccccc1
-            1ccccccccccccccccccccccccccccccccccccccccccccccc1
-            1cccccccccccccaaaaaaaccccccccccccccccccccccccccc1
-            1ccccccccccccacccccccaccccccccccaaaaaccccccccccc1
-            1cccccccccccacccccccccaccccccccacccccacccccccccc1
-            1ccccccccccacccccccccccacccccccacccccacccccccccc1
-            1ccccccccccacccccccccccacccccccacccccacccccccccc1
-            .1cccccccccacccccccccccacccccccacccccaccccccccc1.
-            .1cccccccccacccccccccccacccccccacccccaccccccccc1.
-            ..1ccccccccacccccccccccaccccccccaaaaaccccccccc1..
-            ..1ccccccccacccccccccccacccccccccccccccccccccc1..
-            ...1cccccccacccccccccccaccccccccccccccccccccc1...
-            ...1ccccccccacccccccccacccccccccccccccccccccc1...
-            ....1ccccccccacccccccacccccccccccccccccccccc1....
-            ....1cccccccccaaaaaaaccccccccccccccccccccccc1....
-            .....1ccccccccccccccccccccccccccccccccccccc1.....
-            ......1ccccccccccccccccccccccccccccccccccc1......
-            .......1ccccccccccccccccccccccccccccccccc1.......
-            ........1ccccccccccccccccccccccccccccccc1........
-            .........11ccccccccccccccccccccccccccc11.........
-            ...........11ccccccccccccccccccccccc11...........
-            .............11ccccccccccccccccccc11.............
-            ...............11ccccccccccccccc11...............
-            .................111111111111111.................
-            `, SpriteKind.boss)
-        bigboi.setPosition(75, -20)
-        bigboi.setVelocity(0, 10)
-        bigboi.setFlag(SpriteFlag.AutoDestroy, true)
-        bigboiHP = 10
+        createBigboi()
     } else {
-        asteroide = sprites.create(img`
-            . . . . . . . . . . . . . . . . 
-            . . b b . . 1 1 1 1 1 . . . b . 
-            . . b b . 1 b b b b b 1 . . . . 
-            . . . . 1 b b b b b b b 1 . . . 
-            . . . 1 b b c b b b b b b 1 . . 
-            . . 1 b b b b b b c b b b b 1 . 
-            . . 1 b b b b b b b b b b b 1 . 
-            . . 1 b b c b b b b b b b b 1 . 
-            . . 1 b b c c b b b b b b b 1 . 
-            . . 1 b b b b b b b b c b b 1 . 
-            . . . 1 b b b b b b c c b 1 . . 
-            . . . . 1 b b b b b b b 1 . . . 
-            . . b b . 1 b b b b b 1 . . b . 
-            . . . b . . 1 1 1 1 1 . b b b . 
-            . . . . . . . . . . . . . . . . 
-            . . . . . . . . . . . . . . . . 
-            `, SpriteKind.Enemy)
-        asteroide.setFlag(SpriteFlag.AutoDestroy, true)
-        asteroidLogic(asteroide)
+        createAsteroid()
     }
 })
